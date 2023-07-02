706a3a5fe57aa5dc10765ade5ea51741b6fdfaf9
Nishan shrestha <89296135+battulalshrestha@users.noreply.github.com>
2023-07-02 08:26:50 -0500

    simple ifelse program

---
+++ hello-world/datetime.py
@@ -0,0 +1,7 @@
+class hello:
+ def message():
+   print("my name is nishan shrestha")
+   print(" i live in rautepani")
+   print(" i am studying ncit college")
+ message()
+print(hello)
---
+++ hello-world/iflesle.py
@@ -0,0 +1,8 @@
+a = 23
+b= 34
+if a>b:
+   print("a is greater")
+elif(a==b):
+   print("a and b is equal")
+else:
+   print(" b is greater")
