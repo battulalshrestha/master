084fe4356e0076edce72d793d22abd2e3450f673
Nishan shrestha <89296135+battulalshrestha@users.noreply.github.com>
2023-07-02 10:16:13 -0500

    transforming text

---
+++ hello-world/string2.py
@@ -0,0 +1,8 @@
+print("the moon lies out of surface".title())
+print("out" in "the moon lies out of surface")
+name = 'my name is nishan kumar prasad shrestha'
+print(name.find('is'))
+print(name.count('nishan'))
+print(name.lower())
+print(name.upper())
+print
---
+++ hello-world/transfor_text.py
@@ -0,0 +1,5 @@
+# transfor text by using string method
+# split work the transfer of the sentence 
+text = "my name is nishan shrestha"
+sent = text.split(".")
+print(sent)
